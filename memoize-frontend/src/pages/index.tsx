8e2b643370a4455eb78dd214f12d4374ef4e7362
cjohnson <collin.g.johnson@gmail.com>
2023-02-24 00:22:45 -0500

    Add multiple choice options

--- memoize-frontend/src/pages/index.tsx
+++ memoize-frontend/src/pages/index.tsx
@@ -6,11 +6,26 @@ type CardProps = {
   prompt: string
 }
 
-export const Card = ({ prompt: paragraph }: CardProps) => <aside>
+export const Card = ({ prompt: prompt }: CardProps) => <aside>
   <div className="max-w-sm rounded-lg overflow-hidden shadow-lg m-4 bg-white">
     <div className="px-6 py-4">
       <p className="text-gray-700 text-base">
-        { paragraph }
+        { prompt }
+      </p>
+    </div>
+  </div>
+</aside>
+
+type MultipleChoiceOptionProps = {
+  title: string,
+  option_index: string
+}
+
+export const MultipleChoiceOption = ({ title: title, option_index: option_index }: MultipleChoiceOptionProps) => <aside>
+  <div className="max-w-sm rounded-lg overflow-hidden shadow-lg m-4 bg-white">
+    <div className="px-6 py-4">
+      <p className="text-gray-700 text-base">
+        { option_index }: {title}
       </p>
     </div>
   </div>
@@ -24,8 +39,14 @@ const Home: NextPage = () => {
         <meta name="description" content="memorization made easy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-200 flex justify-center items-center h-screen">
-        <Card prompt="What is the electron geometry of a central atom with 5 electron pairs, 2 bonding pairs, and 3 lone pairs?"></Card>
+      <main className="bg-gray-200 flex justify-center items-center space-between h-screen">
+        <Card prompt="What is the electron geometry of a central atom with 5 electron pairs, 2 bonding pairs, and 3 lone pairs?"/>
+        <div className="columns">
+          <MultipleChoiceOption option_index="A" title="Test 1"/>
+          <MultipleChoiceOption option_index="B" title="Test 2"/>
+          <MultipleChoiceOption option_index="C" title="Test 3"/>
+          <MultipleChoiceOption option_index="D" title="Test 4"/>
+        </div>
       </main>
     </>
   );
