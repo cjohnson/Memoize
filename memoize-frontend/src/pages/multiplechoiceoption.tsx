15c77095fa26c90cfb8463df779eadf9c8e1c5b3
cjohnson <collin.g.johnson@gmail.com>
2023-02-24 00:44:12 -0500

    Add click events for the multiple choice buttons

--- memoize-frontend/src/pages/multiplechoiceoption.tsx
+++ memoize-frontend/src/pages/multiplechoiceoption.tsx
@@ -6,11 +6,21 @@ type MultipleChoiceOptionProps = {
 }
 
 class MultipleChoiceOption extends React.Component<MultipleChoiceOptionProps> {
+  constructor(props: Readonly<MultipleChoiceOptionProps>) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (): void => {
+    console.log('Clicked ' + this.props.title);
+  }
+  
   render() {
     return (
       <>
         <div className="max-w-sm rounded-lg overflow-hidden shadow-lg m-4 bg-white">
-          <a href='#'>
+          <a href='#' onClick={this.handleClick}>
             <div className="px-6 py-4">
               <p className="text-gray-700 text-base">
                 { this.props.option_index }: { this.props.title }
