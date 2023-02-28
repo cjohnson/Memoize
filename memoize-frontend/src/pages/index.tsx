75187bfc0ae5fc0cef08a7310168a65cc3e4c0ca
cjohnson <collin.g.johnson@gmail.com>
2023-02-27 22:55:10 -0500

    Convert to array type to prepare for passing to the QuestionServer

--- memoize-frontend/src/pages/index.tsx
+++ memoize-frontend/src/pages/index.tsx
@@ -5,16 +5,18 @@ import MultipleChoiceQuestion from "@component/components/MultipleChoiceQuestion
 import { MultipleChoiceQuestionData } from "@component/components/QuestionServer/QuestionServer";
 import { MultipleChoiceAnswer } from "@component/components/MultipleChoiceQuestion/MultipleChoiceQuestion";
 
-const question: MultipleChoiceQuestionData = {
-  questionType: 'multiple_choice',
-  prompt: 'What is the electron geometry of a central atom with 5 electron pairs, 2 bonding pairs, and 3 lone pairs?', 
-  answers: Array<MultipleChoiceAnswer>(
-    { label: "Tetrahedral", correct: false, explanation: "A central atom with a tetrahedral electron geometry would have 4 electron groups." },
-    { label: "Trigonal bipyramidal", correct: true, explanation: "A central atom with a trigonal bipyramidal electron geometry would have 5 electron groups." },
-    { label: "Trigonal planar", correct: false, explanation: "A central atom with a trigonal planar electron geometry would have 3 electron groups." },
-    { label: "Trigonal pyramidal", correct: false, explanation: "A central atom with a trigonal pyramidal molecular geometry would have a tetrahedral electron geometry, and thus have 4 electron groups." },
-  ),
-};
+const questions: Array<MultipleChoiceQuestionData> = [
+  {
+    questionType: 'multiple_choice',
+    prompt: 'What is the electron geometry of a central atom with 5 electron pairs, 2 bonding pairs, and 3 lone pairs?', 
+    answers: Array<MultipleChoiceAnswer>(
+      { label: "Tetrahedral", correct: false, explanation: "A central atom with a tetrahedral electron geometry would have 4 electron groups." },
+      { label: "Trigonal bipyramidal", correct: true, explanation: "A central atom with a trigonal bipyramidal electron geometry would have 5 electron groups." },
+      { label: "Trigonal planar", correct: false, explanation: "A central atom with a trigonal planar electron geometry would have 3 electron groups." },
+      { label: "Trigonal pyramidal", correct: false, explanation: "A central atom with a trigonal pyramidal molecular geometry would have a tetrahedral electron geometry, and thus have 4 electron groups." },
+    ),
+  }
+];
 
 const Home: NextPage = () => {
   return (
@@ -24,7 +26,7 @@ const Home: NextPage = () => {
         <meta name="description" content="memorization made easy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MultipleChoiceQuestion index={1} prompt={question.prompt} answers={question.answers} />
+      <MultipleChoiceQuestion index={1} prompt={questions[0]?.prompt ?? ''} answers={questions[0]?.answers ?? []} />
     </>
   );
 };
