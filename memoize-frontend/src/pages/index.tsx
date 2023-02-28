19e97088c06bb93a957c34963f12c87c32ee1a34
cjohnson <collin.g.johnson@gmail.com>
2023-02-27 20:05:57 -0500

    Correct content typo

--- memoize-frontend/src/pages/index.tsx
+++ memoize-frontend/src/pages/index.tsx
@@ -7,7 +7,7 @@ const question = {
   prompt: 'What is the electron geometry of a central atom with 5 electron pairs, 2 bonding pairs, and 3 lone pairs?', 
   answers: [
     { label: "Tetrahedral", correct: false, explanation: "A central atom with a tetrahedral electron geometry would have 4 electron groups." },
-    { label: "Trigonal bipyramidal", correct: true, explanation: "A central atom with a trigonal pyramidal electron geometry would have 5 electron groups." },
+    { label: "Trigonal bipyramidal", correct: true, explanation: "A central atom with a trigonal bipyramidal electron geometry would have 5 electron groups." },
     { label: "Trigonal planar", correct: false, explanation: "A central atom with a trigonal planar electron geometry would have 3 electron groups." },
     { label: "Trigonal pyramidal", correct: false, explanation: "A central atom with a trigonal pyramidal molecular geometry would have a tetrahedral electron geometry, and thus have 4 electron groups." },
   ],
