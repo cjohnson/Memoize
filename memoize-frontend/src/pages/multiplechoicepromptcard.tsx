a2afb16b3aeaf2f762ba7bbd220c087a9e9f3918
cjohnson <collin.g.johnson@gmail.com>
2023-02-27 19:52:03 -0500

    Add numbering to question prompt

--- memoize-frontend/src/pages/multiplechoicepromptcard.tsx
+++ memoize-frontend/src/pages/multiplechoicepromptcard.tsx
@@ -11,6 +11,7 @@ class MultipleChoicePromptCard extends React.Component<MultipleChoicePromptCardP
         <div className="max-w-xl rounded-lg overflow-hidden m-4">
           <div>
             <h1 className="text-gray-700 text-xl">
+              <strong>1. </strong>
               { this.props.prompt }
             </h1>
           </div>
